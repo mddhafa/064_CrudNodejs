4920d0d3f354f2d5c58b5d9a37aa094519e1d558
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-10-20 00:35:33 +0700

    membuat objek baru dan menambahkan atributnya

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mobilRoute from "./router/mobil.js";
-
+import motorRoute from "./router/motor.js";
 
 const app = express();
 const PORT = 8000;
 
 app.use(bodyParser.json());
 
+app.use("/motor", motorRoute);
 app.use("/mobil", mobilRoute);
 app.get("/", (req, res) => {
     console.log(["GET ROUTE"]);
--- router/motor.js
+++ router/motor.js
@@ -1,2 +1,23 @@
-import express, {Router} from express;
+import express, { Router } from "express";
 
+const router = express.Router();
+const motor = [
+    {
+        Merk : "Yamaha",
+        Model : "Vixion",
+        Pajak : "Hidup",
+        Tahun : 2010,
+    },
+    {
+        Merk : "Honda",
+        Model : "Astrea",
+        Pajak : "Mati",
+        Tahun : 2005,
+    }
+];
+
+router.get ("/", (req, res)=>{
+    res.send(motor);
+})
+
+export default router;
